ed0227403b0bc7968e7c93114caeb074f36c6b78
ErtYwek <34419000+ErtYwek@users.noreply.github.com>
2018-04-24 21:49:10 +0200

    BEGIN2

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -1,7 +1,7 @@
 // Copyright (c) 2011-2016 The Cryptonote developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
+// Copyright (c) 2018 LTDtm Natural Leaderz and LTD Community
 #pragma once
 
 #include <cstdint>
